6ca135f57291b64fa9879e78b93f8da88e102c5a
Anderson Udokoro <andersonudokoro@yahoo.com>
2020-11-24 17:23:27 +0100

    Add files via upload

---
+++ ingest_kafka_server.py
@@ -0,0 +1,34 @@
+import logging
+from pyspark.sql import SparkSession
+
+
+def run_spark_job(spark):
+    #TODO set this entry point so that you can start ingesting kafka data
+    df = spark \
+         .readStream \
+         .format("kafka") \
+         .option("kafka.bootstrap.servers","localhost:9092") \
+         .option("subscribe","Uber_Data") \
+         .conf
+         .load()
+    
+
+    # Show schema for the incoming resources for checks
+    print("testing 1,2,3")
+    #df.printSchema()
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+
+    spark = SparkSession \
+        .builder \
+        .master("local[*]") \
+        .appName("StructuredStreamingSetup") \
+        .getOrCreate()
+
+    logger.info("Spark started")
+
+    run_spark_job(spark)
+
+    spark.stop()
